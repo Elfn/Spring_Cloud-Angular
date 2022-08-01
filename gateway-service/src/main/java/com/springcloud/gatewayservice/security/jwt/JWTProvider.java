dfc6782dc4faf245979145c34b06418cdcc70094
elfof <Elimane42@gmail.com>
2022-08-01 14:35:43 +0200

    [JWT Helpers Methods] GEt authentication from Request. Closes #5

--- gateway-service/src/main/java/com/springcloud/gatewayservice/security/jwt/JWTProvider.java
+++ gateway-service/src/main/java/com/springcloud/gatewayservice/security/jwt/JWTProvider.java
@@ -1,8 +1,13 @@
 package com.springcloud.gatewayservice.security.jwt;
 
 import com.springcloud.gatewayservice.security.UserPrincipal;
+import org.springframework.security.core.Authentication;
+
+import javax.servlet.http.HttpServletRequest;
 
 public interface JWTProvider {
 
     String generateToken(UserPrincipal auth);
+
+    Authentication getAuthentication(HttpServletRequest request);
 }
--- gateway-service/src/main/java/com/springcloud/gatewayservice/security/jwt/JWTProviderImpl.java
+++ gateway-service/src/main/java/com/springcloud/gatewayservice/security/jwt/JWTProviderImpl.java
@@ -1,16 +1,24 @@
 package com.springcloud.gatewayservice.security.jwt;
 
 import com.springcloud.gatewayservice.security.UserPrincipal;
+import com.springcloud.gatewayservice.utils.SecurityUtils;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletRequest;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Component
@@ -39,6 +47,52 @@ public class JWTProviderImpl implements JWTProvider {
                 .setExpiration(new Date(System.currentTimeMillis() + JWT_EXPIRATION_IN_MS))
                 .signWith(key, SignatureAlgorithm.ES512)
                 .compact();
+
+    }
+
+    @Override
+    public Authentication getAuthentication(HttpServletRequest request){
+        Claims claims = extractClaims(request);
+        if(claims == null){
+            return null;
+        }
+
+        String username = claims.getSubject();
+        Long userId = claims.get("userId", Long.class);
+
+        Set<GrantedAuthority> authorities = Arrays.stream(claims.get("roles").toString().split(","))
+                .map(SecurityUtils::convertToAuthority)
+                .collect(Collectors.toSet());
+
+        UserDetails userDetails = UserPrincipal.builder()
+                .username(username)
+                .authorities(authorities)
+                .id(userId)
+                .build();
+
+        if(username == null){
+            return null;
+        }
+
+        return new UsernamePasswordAuthenticationToken(userDetails, null, authorities);
+
+
+    }
+
+    private Claims extractClaims(HttpServletRequest request){
+        String token = SecurityUtils.extractAuthTokenFromRequest(request);
+
+        if(token == null){
+            return null;
+        }
+
+        Key key = Keys.hmacShaKeyFor(JWT_SECRET.getBytes(StandardCharsets.UTF_8));
+
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
 
     }
 
