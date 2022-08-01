f1dddbfc9a43e3c0ab3204bcd7ed5aefa33971fd
elfof <Elimane42@gmail.com>
2022-08-01 15:35:20 +0200

    Implementing JWT Authentication Filter. Closes #7

--- gateway-service/src/main/java/com/springcloud/gatewayservice/security/SecurityConfig.java
+++ gateway-service/src/main/java/com/springcloud/gatewayservice/security/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -40,6 +41,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeHttpRequests()
                 .antMatchers("/api/authentication/**").permitAll()
                 .anyRequest().authenticated();
+
+        //To say that jwtAuthorizationFilter is the first filter of the chain
+        http.addFilterBefore(jwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+
     }
 
     @Override
